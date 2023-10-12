b65de123d41caea05b1c623c6c824299888d368d
Kiryl Liavonau <kliavonau@ibagroup.eu>
2023-10-12 18:39:38 +0300

    test corrected

---
+++ tests/test_status.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from app.main import app
+from app.config import settings
+
+
+def test_status():
+    client = TestClient(app)
+    result = client.get("/")
+    assert result.status_code == 200
+    assert result.json() == {"status": "ok"}
